7d8434615d73f29b1407f77ed34cd17cdecfbd43
陈利红 <xiaodachi275@gmail.com>
2023-05-13 15:18:36 +0800

    完成了剥离，且完成了头部板块数据渲染的函数封装

--- js/index.js
+++ js/index.js
@@ -7,7 +7,28 @@ logout('login.html')
 // 进入页面渲染
 async function render() {
   const res = await axios({ url: '/dashboard' })
-  const { overview } = res.data.data
-  Object.keys(overview).forEach(key => (document.querySelector(`.${key}`).innerHTML = overview[key]))
+  const { overview } = res
+
+  renderHeaderData(overview)
 }
 render()
+function renderHeaderData(overview) {
+  Object.keys(overview).forEach(key => (document.querySelector(`.${key}`).innerHTML = overview[key]))
+}
+const line = echarts.init(document.querySelector('#line'))
+const option = {
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  },
+  yAxis: {
+    type: 'value',
+  },
+  series: [
+    {
+      data: [820, 932, 901, 934, 1290, 1330, 1320],
+      type: 'line',
+      areaStyle: {},
+    },
+  ],
+}
